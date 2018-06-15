b8ca0896f7076de8100092c7e42612928b9b2b95
shaggyhiggins <shaggyhiggins@gmail.com>
2018-06-15 14:24:29 -0300

    Stripped mainline to hello world. practice staging file in git

--- simple-cmake/sandbox.cpp
+++ simple-cmake/sandbox.cpp
@@ -1,53 +1,10 @@
+/**
+ *  This is an application that says hello.
+ */
 #include <iostream>
-
-struct Header
-{
-    int _timestamp {0};
-};
-
-struct Data
-{
-    char *data;
-};
-
-class IHandler
-{
-    virtual void handleHeader() = 0;
-    virtual void handleData() = 0;
-};
-
-class StreamHandler : public IHandler
-{
-    virtual void handleHeader()
-    {
-
-    }
-    virtual void handleData()
-    {
-
-    }
-};
-
-class Streamer
-{
-public:
-    void registerHandler(IHandler *handler)
-    {
-        _handler = handler;
-    }
-
-private:
-    IHandler *_handler;
-};
 
 int main(int argc, char const *argv[])
 {
     std::cout << "Hello, Cruel World!" << std::endl;
-
-    StreamHandler handler;
-    Streamer stream;
-
-    stream.registerHandler(&handler);
-
     return 0;
 }
